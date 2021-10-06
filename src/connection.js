587706420fe7eb15dbadc3f54adff7bdcb9d5b16
cesar <cesarspider@hotmail.com>
2021-10-06 01:51:02 -0500

    make it better

--- src/connection.js
+++ src/connection.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 const { mongodb } = require('./config')
 
-const connection = mongoose.connect(`mongodb+srv://chat:chat@chat.qfcap.mongodb.net/chat?retryWrites=true&w=majority`)
+const connection = mongoose.connect(`mongodb://chat:chat@chat.qfcap.mongodb.net/chat?retryWrites=true&w=majority`)
 .then((db)=> {
     console.log('Conexión exitosa')
 }).catch((err)=> {
