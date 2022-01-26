56bcdcbe5422f39e9a71ea6bc0a21c88846b15ae
cesar <cesarspider@hotmail.com>
2022-01-26 01:38:38 -0600

    actualizacion

--- src/index.js
+++ src/index.js
@@ -3,26 +3,19 @@ const app = express()
 //const port=3000
 const cors =require('cors')
 const path = require('path')
-const connection = require('./connection')
 const user=require('./routes/users.js')
 const jq=require('../node_modules/jquery/dist/jquery')
-const loggedMiddleware = require('./middleware/logged.js')
-const socketio = require('socket.io')
 const http = require('http')
 const server = http.createServer(app)
-const io=socketio(server)
 const fs=require('fs')
 
 const mediaserver=require('mediaserver')
 
-require('./socket')(io)
-
 app.set('title', 'aplicacion hecha en node.js')
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 4000)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname,'views'))
 //seccion de middleware
-app.use(loggedMiddleware.isLogged)
 app.use(express.static(path.join(__dirname, 'public')))
 //app.use(cors())
 app.use(express.urlencoded({extended:false}))
@@ -38,18 +31,3 @@ server.listen(app.get('port'),()=>{
 })
 
 app.use('/jquery',jq)
-//
-   // app.use('/jquery',express.static(path.join(__dirname,'node_modules','jquery','dist')))
-    app.get('/canciones',function(req,res){
-        fs.readFile(path.join(__dirname,'canciones.json'),'utf8',function(err,canciones){
-            if(err) throw err;
-            res.json(JSON.parse(canciones));
-        })
-    })
-
-
-    app.get('/canciones/:nombre',function(req,res){
-        var cancion = path.join(__dirname,'canciones',req.params.nombre);
-        mediaserver.pipe(req,res,cancion);
-    })
-//
